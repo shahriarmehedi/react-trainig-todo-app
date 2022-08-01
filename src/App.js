c8a343fd78862a9b3c212fd8729926d260307ab0
shahriarmehedi <sau.shahriar@gmail.com>
2022-08-01 18:56:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const todoData = [
+
+  ]
+
+  console.log(todos);
+
+
+  // BUTTON FUNCTION
+  const addTodo = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: todoData.length + 1,
+      text: todo
+    }
+    setTodo('');
+    todoData.push(newTodo);
+    setTodos([...todos, newTodo]);
+  }
+
+
+  // ONCHNAGE FUNCTION
+  const onchange = (e) => {
+    setTodo(e.target.value);
+    console.log(e.target.value);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h3>Todo Application</h3>
+
+      <input onChange={onchange} type="text" />
+      <button onClick={addTodo}>Add</button>
+
+      <div>
+        {
+          todos.map(todo => {
+            return <div key={todo.id}>{todo.text}</div>
+          }
+          )
+        }
+      </div>
     </div>
   );
 }
